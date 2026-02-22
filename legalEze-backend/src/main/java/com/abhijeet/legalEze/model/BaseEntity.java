2b42d4b4b84727ce4b940dff5814bd927b010ccf
abhijeet4725 <abhijeetcs4725@gmail.com>
2026-02-22 12:52:12 +0530

    feat: configure BaseEntity with Spring Data JPA auditing and UUID id

--- legalEze-backend/src/main/java/com/abhijeet/legalEze/model/BaseEntity.java
+++ legalEze-backend/src/main/java/com/abhijeet/legalEze/model/BaseEntity.java
@@ -1,4 +1,31 @@
 package com.abhijeet.legalEze.model;
 
-public class BaseEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
